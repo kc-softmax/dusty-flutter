75925a86b8160ed1557e83875d2f84a00e888f02
n1sz <n1szkjh@gmail.com>
2024-01-30 14:24:42 +0900

    [UPDATE] initial state

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:dusty_flutter/flame_texturepacker.dart';
+
+class Player extends SpriteAnimationComponent
+    with HasGameRef<SpaceShooterGame> {
+  Player()
+      : super(
+          size: Vector2(48, 48),
+          anchor: Anchor.center,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    final atlas = await gameRef.fromAtlas('images/test.atlas');
+    final spriteList = atlas.findSpritesByName('yellow_body');
+    animation = SpriteAnimation.spriteList(
+      spriteList,
+      stepTime: 0.05,
+    );
+
+    position = game.size / 4;
+  }
+
+  void move(Vector2 delta) {
+    position.add(delta);
+  }
+}
+
+class SpaceShooterGame extends FlameGame with PanDetector {
+  late Player player;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    player = Player();
+    add(player);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    player.move(info.delta.global);
+  }
+}
+
+void main() {
+  runApp(GameWidget(game: SpaceShooterGame()));
+}
