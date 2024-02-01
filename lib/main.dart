05865f61e7dd4021da1d550fc3608e5c610f3819
n1sz <n1szkjh@gmail.com>
2024-02-01 12:30:55 +0900

    [UPDATE] merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,135 @@
 import 'package:dusty_flutter/arbiter/arbiter_client.dart';
 import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flutter/material.dart';
+import 'package:dusty_flutter/atlas/texture_atlas.dart';
 import 'package:dusty_flutter/flame_texturepacker.dart';
+import 'package:dusty_flutter/characters/dusty.dart';
 
-class Player extends SpriteAnimationComponent
-    with HasGameRef<SpaceShooterGame> {
-  Player()
-      : super(
-          size: Vector2(48, 48),
-          anchor: Anchor.center,
-        );
+// @override
+// void onCollisionStart(
+//   Set<Vector2> intersectionPoints,
+//   PositionComponent other,
+// ) {
+//   super.onCollisionStart(intersectionPoints, other);
+// }
+
+class DustyIsland extends FlameGame with HasCollisionDetection {
+  late final JoystickComponent joystick;
+  late final TiledComponent mapComponent;
+  late final TextureAtlas atlas;
+  late final Dusty player;
 
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
+    // set hud camera
 
-    final atlas = await gameRef.fromAtlas('images/test.atlas');
-    final spriteList = atlas.findSpritesByName('yellow_body');
-    animation = SpriteAnimation.spriteList(
-      spriteList,
-      stepTime: 0.05,
+    atlas = await fromAtlas('images/dusty-island.atlas');
+    mapComponent = await TiledComponent.load('map.tmx', Vector2.all(64));
+    world.add(mapComponent);
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: atlas.findSpriteByName('knob'),
+        size: Vector2.all(size.y * 0.15),
+      ),
+      background: SpriteComponent(
+        sprite: atlas.findSpriteByName('joystick_bg'),
+        size: Vector2.all(size.y * 0.35),
+      ),
+      margin: const EdgeInsets.only(left: 44, bottom: 44),
     );
 
-    position = game.size / 4;
-  }
+    player = Dusty();
 
-  void move(Vector2 delta) {
-    position.add(delta);
-  }
-}
+    final buttonSize = Vector2(56, 59);
+    final smallButtonSize = Vector2(44, 46);
 
-class SpaceShooterGame extends FlameGame with PanDetector {
-  late Player player;
+    final shieldButton = HudButtonComponent(
+      button: SpriteComponent(
+        sprite: atlas.findSpriteByName('circle_button'),
+        size: buttonSize,
+      ),
+      buttonDown: SpriteComponent(
+        sprite: atlas.findSpriteByName('press_circle_button'),
+        size: buttonSize,
+      ),
+      margin: const EdgeInsets.only(
+        right: 68,
+        bottom: 65,
+      ),
+      onPressed: player.flipHorizontally,
+    );
 
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-    player = Player();
-    add(player);
-    overlays.add("TestButton");
-  }
+    @override
+    Future<void> onLoad() async {
+      await super.onLoad();
+      player = Player();
+      add(player);
+      overlays.add("TestButton");
+    }
 
-  @override
-  void onPanUpdate(DragUpdateInfo info) {
-    player.move(info.delta.global);
+    final boostButton = HudButtonComponent(
+      button: SpriteComponent(
+        sprite: atlas.findSpriteByName('small_circle_button'),
+        size: smallButtonSize,
+      ),
+      buttonDown: SpriteComponent(
+        sprite: atlas.findSpriteByName('press_small_circle_button'),
+        size: smallButtonSize,
+      ),
+      margin: const EdgeInsets.only(
+        right: 144,
+        bottom: 38,
+      ),
+      // onPressed: player.flipHorizontally,
+    );
+
+    final activeButton = HudButtonComponent(
+      button: SpriteComponent(
+        sprite: atlas.findSpriteByName('small_circle_button'),
+        size: smallButtonSize,
+      ),
+      buttonDown: SpriteComponent(
+        sprite: atlas.findSpriteByName('press_small_circle_button'),
+        size: smallButtonSize,
+      ),
+      margin: const EdgeInsets.only(
+        right: 144,
+        bottom: 108,
+      ),
+      // onPressed:  ,
+    );
+
+    final specialButton = HudButtonComponent(
+      button: SpriteComponent(
+        sprite: atlas.findSpriteByName('small_circle_button'),
+        size: smallButtonSize,
+      ),
+      buttonDown: SpriteComponent(
+        sprite: atlas.findSpriteByName('press_small_circle_button'),
+        size: smallButtonSize,
+      ),
+      margin: const EdgeInsets.only(
+        right: 88,
+        bottom: 150,
+      ),
+      onPressed: player.flipHorizontally,
+    );
+
+    world.addAll([player]);
+    camera.follow(player);
+    camera.viewport.addAll([
+      joystick,
+      shieldButton,
+      boostButton,
+      activeButton,
+      specialButton,
+    ]);
   }
 }
 
@@ -67,4 +151,5 @@ void main() {
       }
     },
   ));
+  runApp(GameWidget(game: DustyIsland()));
 }
