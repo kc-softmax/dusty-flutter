00043ab0f8587cb633db220bbc8ba7a29853ead9
jaehong0721 <newbie302@gmail.com>
2024-02-05 12:29:20 +0900

    [ADD] shared preference 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:dusty_flutter/arbiter/api/models.dart';
+import 'package:dusty_flutter/scenes/play_scene.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:dusty_flutter/game.dart';
 import 'package:dusty_flutter/widgets/splash_screen.dart';
 import 'package:dusty_flutter/arbiter/arbiter_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DustyIslandApp extends StatelessWidget {
   const DustyIslandApp({super.key});
@@ -21,11 +24,46 @@ GameWidget<DustyIslandGame> buildGame() {
   return GameWidget.controlled(
     gameFactory: DustyIslandGame.new,
     overlayBuilderMap: {
+      "TestInputNickname": (context, game) {
+        final controller = TextEditingController();
+        return Scaffold(
+          body: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  height: 36,
+                  child: TextField(
+                    controller: controller,
+                  ),
+                ),
+                FilledButton(
+                    onPressed: () async {
+                      final result = await Arbiter.api.loginByUserName(
+                          RequestLoginByUserName(
+                              userName: controller.value.text));
+                      final prefs = await SharedPreferences.getInstance();
+                      await prefs.setString("token", result.accessToken);
+                      game.overlays.remove('TestInputNickname');
+                      game.router.pushReplacementNamed(PlayScene.routerName);
+                    },
+                    child: const Text("시작")),
+              ],
+            ),
+          ),
+        );
+      },
       "TestButton": (BuildContext context, DustyIslandGame game) {
         return FilledButton(
-            onPressed: () {
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              final token = prefs.getString("token");
               Arbiter.liveService.on(
-                "/di/ws?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDcxMDQyMzYsInN1YiI6IjE1In0.SqJrXhDWl-eQVMyY1m02dm8e1OZqOV1iSY0LSkkP0i0",
+                "/di/ws?token=$token",
                 (message) {
                   debugPrint(message.toString());
                 },
