439224125e27d159c4cb6989e056e28488c3e241
jaehong0721 <newbie302@gmail.com>
2024-03-05 17:04:11 +0900

    [ADD] 랭킹 테이블 와이어프레임 추가

--- lib/arbiter/live_service/mobile.dart
+++ lib/arbiter/live_service/mobile.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:dusty_flutter/arbiter/live_service/base.dart';
+import 'package:flutter/widgets.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ArbiterLiveService extends BaseArbiterLiveService {
@@ -29,12 +30,23 @@ class ArbiterLiveService extends BaseArbiterLiveService {
   }
 
   @override
-  void sendByte(ByteBuffer message) {
-    _channel.sink.add(message.asUint8List());
+  void sendByte(ByteBuffer message) async {
+    // NOTE
+    // websocket의 연결 상태를 확인 후 처리하고 싶으나
+    // 라이브러리에서 연결 상태 값을 가져올 수 없어 try-catch로 처리
+    try {
+      _channel.sink.add(message.asUint8List());
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   @override
-  void close() {
-    _channel.sink.close();
+  void close() async {
+    try {
+      await _channel.sink.close();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
