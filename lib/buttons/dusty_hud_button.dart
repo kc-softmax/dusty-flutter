6304407f3c7f232cca75f87848ea933a3c854fd6
jaehong0721 <newbie302@gmail.com>
2024-03-06 18:55:31 +0900

    [ADD] 롱탭으로 게이지를 충전하는 버튼 추가

--- lib/buttons/dusty_hud_button.dart
+++ lib/buttons/dusty_hud_button.dart
@@ -1,6 +1,8 @@
 import 'dart:math' as math;
+import 'dart:math';
 import 'package:dusty_flutter/arbiter/live_service/game_message.dart';
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -84,6 +86,7 @@ class DustyHudButton extends HudButtonComponent {
     }
   }
 
+  @mustCallSuper
   @override
   void update(double dt) {
     if (timer.isRunning()) {
@@ -104,5 +107,61 @@ class DustyHudButton extends HudButtonComponent {
     final sweepAngle = math.pi * 2 - math.pi * 2 * progress;
     final rect = Rect.fromLTWH(0, 0, size.x, size.y);
     canvas.drawArc(rect, startAngle, sweepAngle, true, paint);
+  }
+}
+
+class LongTapDustyHudButton extends DustyHudButton {
+  int longTapMaxTime = 0;
+  bool _isPressing = false;
+  double _pressedTime = 0;
+  // TEMP 게이지는 int형
+  double _guage = 0;
+  Function(double, int) handleLongTapButtonAction;
+
+  LongTapDustyHudButton({
+    super.button,
+    super.buttonDown,
+    super.margin,
+    super.onReleased,
+    super.onCancelled,
+    super.position,
+    super.size,
+    super.scale,
+    super.angle,
+    super.anchor,
+    super.children,
+    super.priority,
+    required super.reloadDuration,
+    required this.longTapMaxTime,
+    required this.handleLongTapButtonAction,
+  });
+
+  @override
+  void onTapUp(TapUpEvent event) {
+    handleLongTapButtonAction(progress, _guage.toInt());
+    _clearGuage();
+    super.onTapUp(event);
+  }
+
+  @override
+  void onLongTapDown(TapDownEvent event) {
+    if (progress != 0) return;
+    _isPressing = true;
+  }
+
+  @override
+  void update(double dt) {
+    if (_isPressing && _guage < 100) {
+      _pressedTime += dt;
+      _guage = min((_pressedTime / longTapMaxTime) * 100, 100);
+      print('preesedTime: $_pressedTime, guage: $_guage');
+    }
+    super.update(dt);
+  }
+
+  void _clearGuage() {
+    _guage = 0;
+    _pressedTime = 0;
+    _isPressing = false;
   }
 }
