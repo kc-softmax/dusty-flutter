662a5e5f55a83daed52b7f77e97cc03680ebcf1a
jaehong0721 <newbie302@gmail.com>
2024-03-15 14:25:41 +0900

    [UPDATE] 배포 스크립트 변경

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo ARBITER_BASE_SOCKET_URL=$ARBITER_BASE_SOCKET_URL > .env && \
 echo ARBITER_BASE_API_URL=$ARBITER_BASE_API_URL >> .env  && \
-if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git -b stable; fi && ls && cd flutter && git checkout 3.19.1 && cd .. && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter pub get
+touch .debug.env && \
+if cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git -b stable; fi && ls && cd flutter && git checkout 3.19.1 && cd .. && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
